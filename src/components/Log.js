85ccd66261b8a0823444d59418e35621b1d73686
Алексей Кассь <akass1985@gmail.com>
2019-11-01 12:48:51 +0500

    Сделал перетаскивание

--- src/components/Log.js
+++ src/components/Log.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import selectResult, { selectBuffer, selectLog } from '../selectors'
 import { hidden, redBright } from 'ansi-colors';
 import Container from 'react-bootstrap/Container'
+// import { ItemTypes } from './Constants'
 
 const Log = () => {
 
@@ -15,18 +16,45 @@ const Log = () => {
     const textarea = document.getElementById('log');
     textarea.scrollTop = textarea.scrollHeight;
   }
+
+  const addDrop = (e) => {
+    // var e = e || window.event; // get window.event if e argument missing (in IE)   
+    if (e.preventDefault) { e.preventDefault(); } // stops the browser from redirecting off to the image.
+
+    var dt    = e.dataTransfer;
+    var files = dt.files;
+    for (var i=0; i<files.length; i++) {
+      var file = files[i];
+      var reader = new FileReader();
+        
+      //attach event handlers here...
+    
+      reader.readAsDataURL(file);
+    }
+    return false;
+  }
+
+  const onDrop = (e) => {
+    var log = document.getElementById("logContent");
+    log.textContent = e.dataTransfer.getData("Text");
+  }
+  const onDragOver = (e) =>  e.target.style.border = "3px solid red";
+  const onDragLeave = (e) => e.target.style.border = "1px solid grey";
   
   return (
       <Container id="log">
           <textarea 
-            id="log"
+            id="logContent"
             rows="5"
             onchange={ () => scroll() }
+            onDragOver={ document.addEventListener("dragover", function(event) {
+              event.preventDefault();
+            }) }
+            onDrop={ e => onDrop(e) }
+            onDragOver={ e => onDragOver(e) }
+            onDragLeave={ e => onDragLeave(e) }
             style={{
-                // border: "none",
-                "text-align": "center",
-                "min-width": "100%",
-                "min-height": "100%",
+                
                 scrollToBottom: true, 
                 overflow: hidden, 
                 resize: "none"}}
